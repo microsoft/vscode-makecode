1bdaf2bfa5a8ef61888ce5af376fb2c448049b16
Joey Wunderlich <joey.wunderlich@gmail.com>
2023-02-14 13:15:31 -0800

    don't check for updates every build

--- src/web/buildWatcher.ts
+++ src/web/buildWatcher.ts
@@ -27,7 +27,6 @@ export class BuildWatcher {
     private constructor(protected context: vscode.ExtensionContext) {
         this.buildOpts = {
             javaScript: true,
-            update: true,
             watch: true
         };
     }
@@ -149,6 +148,10 @@ export class BuildWatcher {
 
                     if (result.diagnostics.length) {
                         reportBuildErrors(result);
+                        for (const errorHandler of this.errorListeners) {
+                            errorHandler(result.diagnostics);
+                        }
+                        return;
                     }
 
                     if (!this.running) {return;}
