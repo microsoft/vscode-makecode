c2adb0203a23b1f5fb119ecb0da6b27a2904b37b
Joey Wunderlich <jwunderl@users.noreply.github.com>
2023-02-22 09:42:34 -0800

    remove the prompt since it's moving towards auto save (#75)

--- resources/assetMessaging.js
+++ resources/assetMessaging.js
@@ -1,6 +1,5 @@
 (function () {
     let frame;
-    let toastContainer;
     let vscode = acquireVsCodeApi();
 
     window.addEventListener("message", function (m) {
@@ -17,27 +16,9 @@
             }
         } else {
             vscode.postMessage(m.data);
-            if (m.data.type === "save") {
-                showToast("Project assets saved!", 3000);
-            }
         }
     });
     document.addEventListener("DOMContentLoaded", function (event) {
         frame = document.getElementById("asset-editor-frame");
-        toastContainer = document.getElementById("toast-container");
     });
-
-    function showToast(message, millis) {
-        clearToastContainer();
-        const toastDiv = document.createElement("div");
-        toastContainer.appendChild(toastDiv);
-
-        toastDiv.textContent = message;
-
-        setTimeout(() => clearToastContainer(), millis)
-    }
-
-    function clearToastContainer() {
-        while (toastContainer.firstChild) toastContainer.firstChild.remove();
-    }
 }())
